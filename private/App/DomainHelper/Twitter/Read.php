1cdc8a36d37465545112d4bfaa802e5f5d33bfac
mrcoderhazem <one_zoma@yahoo.com>
2018-09-09 04:08:30 +0200

    delete some space.

--- private/App/DomainHelper/Twitter/Read.php
+++ private/App/DomainHelper/Twitter/Read.php
@@ -11,18 +11,18 @@
         {
             
             public function __construct(){
-                   $this->command = $this->initCommand();
+                $this->command = $this->initCommand();
             }
             protected function initCommand(){
-                    return CommandFactory::getCommand('twitterApi');
+                return CommandFactory::getCommand('twitterApi');
             }
             /**
              * @method readTimeLine Read time line of oAuth User.
              */
             protected function readTimeLine(array $parameter = []){
-                      return $this->command->execute(["ModelClass"=>"Tweet\\Viewer","Method"=>
-                      ['Name'=>"readTimeLine","parameters"=>[],"user_auth"=>['status'=>true,'access_token'=>$parameter["oauth_token"]
-                      ,'access_token_secret'=>$parameter['oauth_token_secret']]]]);
+                return $this->command->execute(["ModelClass"=>"Tweet\\Viewer","Method"=>
+                ['Name'=>"readTimeLine","parameters"=>[],"user_auth"=>['status'=>true,'access_token'=>$parameter["oauth_token"]
+                ,'access_token_secret'=>$parameter['oauth_token_secret']]]]);
             }
             /**
              * @method searchTweets search tweets for specific words.
